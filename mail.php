2248f20d7fd26f220116ebf15f770ef0b0e8ab7b
swapnali12deokar <swapnalideokar0307@gmail.com>
2023-03-09 14:20:58 +0530

    updated changes

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $number= $_POST['mobile'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
+$to = "hello@iipl.live";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message ."\r\n Mobile number = " . $number;
 $headers = "From: noreply@https://iipl.live/careers.com";
